e95811e4b63964ba16056c791f5d9f4c254a4005
09tails <44311986+09tails@users.noreply.github.com>
2018-10-20 08:32:11 -0500

    Update Program.cs

--- OGStash/Program.cs
+++ OGStash/Program.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -37,60 +37,109 @@ namespace OGStash
             }
             return result;
         }
-        static bool run;
         static ChromeOptions options;
         static ChromeDriverService chromeDriverService;
         static IWebDriver driver;
-        static int sellingcnt;
-        static int introcnt;
-        static int replystxtcount;
-        static int currentreply;
-        static int lastreply = 1;
-        static List<IWebElement> elemlist = new List<IWebElement>();
-        static void Reply()
+        static bool skip;
+        static bool run;
+        static private CustomDiscordRPC.DiscordRpc.RichPresence presence;
+        static CustomDiscordRPC.DiscordRpc.EventHandlers handlers;
+        private static void ReadyCallback()
         {
-            if (currentreply == lastreply)
-            {
-
-            }
-            else
-            {
-                driver.Navigate().GoToUrl("https://ogstash.com/Forum-Introductions");
-                Thread.Sleep(1000);
-                foreach (IWebElement elem in driver.FindElements(By.ClassName(" subject_new")))
-                {
-                    elemlist.Add(elem);
-                    Colorful.Console.WriteLine(elem.Text);
-                }
-                foreach (IWebElement elem1 in elemlist)
-                {
-                    elem1.Click();
-                    Thread.Sleep(2000);
-                    Colorful.Console.WriteLine(driver.FindElement(By.ClassName("post_body")).Text);
-                    driver.Navigate().GoToUrl("https://ogstash.com/Forum-Introductions");
-                }
-            }
-            Colorful.Console.ReadKey();
+            System.Console.WriteLine("Ready");
         }
 
+        private static void DisconnectedCallback(int errorCode, string message)
+        {
+            System.Console.WriteLine("Disconnect " + errorCode.ToString() + ": " + message);
+        }
+        
+        private static void ErrorCallback(int errorCode, string message)
+        {
+            System.Console.WriteLine("Error " + errorCode.ToString() + ": " + message);
+        }
+
+        static string command;
+        static bool notplaying = true;
+        static void DoStuff()
+        {
+            //System.Console.WriteLine(driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/div/a/span[2]")).Text);
+            //System.Console.WriteLine(driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/a")).Text);Thread.Sleep(5000);
+            handlers = new CustomDiscordRPC.DiscordRpc.EventHandlers();
+            handlers.readyCallback = ReadyCallback;
+            handlers.disconnectedCallback += DisconnectedCallback;
+            handlers.errorCallback += ErrorCallback;
+            CustomDiscordRPC.DiscordRpc.RPCInit("503076155507605509", ref handlers, true, null);
+            Colorful.Console.WriteWithGradient<char>($"Enter Command (help for help): \n", Color.Red, Color.Violet, 10);
+            while (true)
+            {
+                try
+                {
+                    if (playing)
+                    {
+
+                    }
+                    else
+                    {
+                        command = System.Console.ReadLine();
+                    }
+
+                    if (command.Contains("https://"))
+                    {
+                        if (playing == false)
+                        {
+                            driver.Navigate().GoToUrl(command);
+                        }
+                        Thread.Sleep(1000);
+                        presence.details = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/div/a/span[2]")).Text;
+                        presence.state = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/a")).Text;
+                        presence.largeImageKey = "image";
+                        CustomDiscordRPC.DiscordRpc.RPCUpdate(ref presence);
+                        if (playing == false)
+                        {
+                            driver.FindElement(By.XPath("//*[@id=\"content\"]/div/div[4]/div/div[2]/div[2]/div/div/div[1]/a")).Click();
+                        }
+                        currengsong = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/div/a/span[2]")).Text;
+                        //https://soundcloud.com/stationsix/imsosickofthis
+                        playing = true;
+                    }
+                    if (command == "skip")
+                    {
+                        driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/button[3]")).Click();
+                        presence.details = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/div/a/span[2]")).Text;
+                        presence.state = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/a")).Text;
+                        presence.largeImageKey = "image";
+                        CustomDiscordRPC.DiscordRpc.RPCUpdate(ref presence);
+                    }
+                    if (command == "help")
+                    {
+                        System.Console.WriteLine("Paste The URL To Play One.\nskip To Skip.");
+                    }
+                    presence.details = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/div/a/span[2]")).Text;
+                    presence.state = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[4]/div/div/div[3]/div[6]/div/div[1]/a")).Text;
+                    presence.largeImageKey = "image";
+                    CustomDiscordRPC.DiscordRpc.RPCUpdate(ref presence);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        static string currengsong;
+        static bool playing;
         static void Main(string[] args)
         {
-            Colorful.Console.WriteWithGradient<char>("OGStash.com Bot | Made By 9tails\n\n", Color.Red, Color.Violet, 10);
-            Colorful.Console.Title = $"OGStash.com Bot | Made By 9tails | Stats: Replied To {sellingcnt} Selling Threads, And {introcnt} Introduction Threads.";
-            replystxtcount = File.ReadAllLines("replys.txt").Count();
-            Colorful.Console.WriteWithGradient<char>($"Amount Of Replys Available To Send: {replystxtcount}\n", Color.Blue, Color.Violet, 10);
+            Colorful.Console.Title = "Soundcloud DRPC | Made By 9tails";
+            Colorful.Console.WriteWithGradient<char>("Soundcloud DRPC | Made By 9tails\n\n", Color.Red, Color.Violet, 10);
             options = new ChromeOptions();
             options.AddArgument("--headless");
             chromeDriverService = ChromeDriverService.CreateDefaultService();
             chromeDriverService.HideCommandPromptWindow = true;
             driver = new ChromeDriver(chromeDriverService);
-            driver.Navigate().GoToUrl("https://ogstash.com/member.php?action=login");
-            Thread.Sleep(10000);
-            driver.FindElement(By.XPath("//*[@id=\"content\"]/div/form/table/tbody/tr[3]/td[2]/input")).SendKeys(ReadLine(@"info.txt", 1));
-            driver.FindElement(By.XPath("//*[@id=\"content\"]/div/form/table/tbody/tr[4]/td[2]/input")).SendKeys(ReadLine(@"info.txt", 2));
-            driver.FindElement(By.XPath("//*[@id=\"content\"]/div/form/div/input")).Click();
-            Thread.Sleep(3000);
-            Reply();
+            Thread thread = new Thread(DoStuff);
+            thread.Start();
         }
     }
 }
